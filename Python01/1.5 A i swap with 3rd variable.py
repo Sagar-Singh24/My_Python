7f63f18fc3ec480d4e79c709686e41ba0fc3f33d
Sagar Singh <sssssagarsingh24@gmail.com>
2024-11-18 02:39:12 +0530

    Add files via upload

---
+++ Python01/1.5 A i swap with 3rd variable.py
@@ -0,0 +1,7 @@
+x=int(input("Enter the value of x -> "))
+y=int(input("Enter the value of y -> "))
+z=x
+x=y
+y=z
+print("The new value of x is -> ",x)
+print("The new value of y is -> ",y)
---
+++ Python01/1.5 Aii swap without variable.py
@@ -0,0 +1,5 @@
+x=int(input("Enter the value of x -> "))
+y=int(input("Enter the value of y -> "))
+x, y = y, x
+print("The new value of x is -> ",x)
+print("The new value of y is -> ",y)

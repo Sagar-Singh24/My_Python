cdc5aea6d6c123a15f74631099c1258a55f06d97
Sagar Singh <sssssagarsingh24@gmail.com>
2024-11-18 02:41:25 +0530

    Add files via upload

---
+++ Python01/1.10 upper & lower case.py
@@ -0,0 +1,6 @@
+name1=input("Enter your name in lower case -> ")
+print("Name in Upper Case ->",name1.upper())
+name2=input("\n\nEnter your name in upper case -> ")
+print("Name in Lower Case ->",name2.lower())
+
+

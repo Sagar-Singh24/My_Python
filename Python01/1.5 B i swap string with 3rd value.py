652bafa1547b365e0de664bd9d5ddef93040ca74
Sagar Singh <sssssagarsingh24@gmail.com>
2024-11-18 02:39:40 +0530

    Add files via upload

---
+++ Python01/1.5 B i swap string with 3rd value.py
@@ -0,0 +1,7 @@
+str1=input("Enter string 1 -> ")
+str2=input("Enter string 2 -> ")
+temp=str1
+str1=str2
+str2=temp
+print("New value of string 1 is -> ",str1)
+print("New value of string 2 is -> ",str2)
---
+++ Python01/1.5 B ii swap string without using 3rd value.py
@@ -0,0 +1,5 @@
+str1=input("Enter string 1 -> ")
+str2=input("Enter string 2 -> ")
+str1,str2=str2,str1
+print("New value of string 1 is -> ",str1)
+print("New value of string 2 is -> ",str2)

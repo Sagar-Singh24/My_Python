5cdd9d32864a4a8ff910c342ea0efacc99bb24cb
Sagar Singh <sssssagarsingh24@gmail.com>
2024-11-18 02:40:04 +0530

    Add files via upload

---
+++ Python01/1.6 area of sphere rectangle triangle circle.py
@@ -0,0 +1,18 @@
+# Area of Rectangle
+l=int(input("Enter the length of the reactangle -> "))
+b=int(input("Enter the breadth of the reactangle -> "))
+ar=l*b
+print("The Area Of given rectangle is -> ",ar)
+# Area of triangle
+ba=int(input("\n\nEnter the base of the triangle ->"))
+h=int(input("Enter the height of the triangle ->"))
+a=(1/2)*ba*h
+print("The Area Of given triangle is -> ",a)
+# Area of circle
+r1=int(input("\n\nEnter the radius of the circle -> "))
+ac=(22/7)*r1*r1
+print("The area of given circle -> ",ac)
+# Area of sphere
+r2=int(input("\n\nEnter the radius of sphere -> "))
+asp=4*(22/7)*r2*r2
+print("The area of given sphere is -> ",asp)
---
+++ Python01/1.7 substring using string slicing.py
@@ -0,0 +1,4 @@
+paragraph = input("Enter a paragraph: ")
+start = int(input("Enter the start index: "))
+end = int(input("Enter the end index: "))
+print("Sub-string:", paragraph[start:end])

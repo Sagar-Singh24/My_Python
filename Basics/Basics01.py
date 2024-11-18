9dcaeddf121e3d907ecb51525e17463dfaabb709
Sagar Singh <sssssagarsingh24@gmail.com>
2024-11-19 03:29:37 +0530

    Add files via upload

---
+++ Basics/Basics01.py
@@ -0,0 +1,24 @@
+# Basic Printing
+print("Sagar Singh")
+print(123456789)
+
+# Taking User Input
+name = input("Enter Your Name:")
+Regno = input("Enter Your Registration No:")
+print(name)
+print(Regno)
+
+# Basic operator
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+# Simple Intrest
+p = int(input("Enter principle: "))
+r = int(input("Enter rate of intrest: "))
+t = int(input("Enter time: "))
+
+print("Simple Intrest is :", (p*r*t)/100)
---
+++ Basics/Basics02.py
@@ -0,0 +1,34 @@
+# Lists
+List1=[1,2,3,4,5,6,7,8,9]
+print(List1[4])
+print(List1[1:8])
+print(List1[::2])
+print(List1[2:9:3])
+
+'''name="Sagar Singh Parmar"
+print("First Name:",name[0:5])
+print("Middle Name:",name[6:11])
+print("Last Name:",name[12:19])'''
+
+# Tuple
+tup1 = ("Sagar","Sahil","Singh")
+print(tup1[0:2])
+
+#Dictionary
+dict1 = {"milli":50,"lilli":40,"jhilli":30}
+print(dict1["jhilli"])
+
+# String
+a="strl "
+print(a*10)
+
+str1= "Aurat Ka Chakkar Babubhaiya"
+print(str1.lower())
+print(str1.upper())
+print(str1.capitalize())
+print(str1.title())
+print(str1.casefold())
+print(str1.replace("Babubhaiya","Shayam"))
+
+dict1 = {1:"a",2:"b",1:"c"}
+print(dict1)
